67683f2646541c14c2a0f3e08a6802066c238c29
Matt <matthew.king@stfc.ac.uk>
2015-07-03 16:13:29 +0100

    classes built in oo_basics main.cpp

--- exercises/matt_king/ex02_oo_basics/src/main.cpp
+++ exercises/matt_king/ex02_oo_basics/src/main.cpp
@@ -3,9 +3,10 @@
 */
 #include <iostream>
 
-template <class S> class ShapeSorter{
+template <class S>class ShapeSorter{
 public:
-	std::string printShapesThatMatchType(string type) const{};
+	ShapeSorter();
+	std::string printShapesThatMatchType(std::string type) const{};
 	std::string printShapesThatMatchSides(int sides) const{};
 	std::string printAreasDesc() const{};
 	std::string printPerimeterDesc() const{};
@@ -14,31 +15,52 @@ private:
 };
 
 class Shape{
+public:
 	Shape();
-	Shape(std::string type);
+	Shape(std::string type){shapeType = type;};
+private:
+	std::string shapeType;
+	double area;
+	double perimeter;
 };
 
 class Square: public Shape{
+public:
 	Square();
 	Square(std::string type, int side);
+private:
+	int side;
 };
 
 class Rectangle: public Shape{
+public:
 	Rectangle();
 	Rectangle(std::string type, int sideOne, int sideTwo);
+private:
+	int sideOne;
+	int sideTwo;
 };
 
 class Circle: public Shape{
+public:
 	Circle();
 	Circle(std::string type, int radius);
+private:
+	int radius;
 };
 
+
 class Triangle: public Shape{
+public:
 	Triangle();
 	Triangle(std::string type, int base, int height);
+private:
+	int base;
+	int height;
 };
 
 int main(int, char **)
 {
+	Shape s("Circle");
 	std::cout << "Hello";
 }
