3a3fe6ef9ab2df720d5835748fe582f2644cb80f
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2015-07-07 09:35:22 +0100

    Changed Triangle perimeter to isoceles calculation

--- exercises/elliot_oram/ex02_oo_basics/src/Triangle.cpp
+++ exercises/elliot_oram/ex02_oo_basics/src/Triangle.cpp
@@ -2,6 +2,7 @@
 #include "Triangle.h"
 
 #include <iostream>
+#include <iomanip>
 #include <math.h>
 #include <string>
 
@@ -14,7 +15,7 @@ Triangle::Triangle(double h, double w):Shape(){
 	setSides(3);
 	setHeight(h);
 	setWidth(w);
-	setPerimeter(h + w + sqrt((h*h) + (w*w))); //Assumed to be right angled triangle
+	setPerimeter(w + sqrt((h*h) + ((w*w)/4))); //Assumed to be isoceles triangle
 	setArea(0.5 * (h*w));
 }
 
@@ -22,13 +23,10 @@ Triangle::~Triangle(){}
 
 //Member Functions
 void Triangle::print(){
-	std::cout << "=========================" << std::endl;
-	std::cout << "Type: " << getType() << std::endl;
-	std::cout << "Area: " << getArea() << std::endl;
-	std::cout << "Perimeter: " << getPerimeter() << std::endl;
-	std::cout << "Height: " << getHeight() << std::endl;
-	std::cout << "Width: " << getWidth() << std::endl;
-	std::cout << "=========================" <<std::endl;
+	Shape::print();
+	std::cout << std::setw(12) << std::left << "Height: " << std::setw(10) << std::left << getHeight() << std::endl;
+	std::cout << std::setw(12) << std::left << "Base: "   << std::setw(10) << std::left << getWidth()  << std::endl;
+	std::cout << "============================================" <<std::endl;
 }
 
 //End of File
