7d16af2b56e1fb31d1dcc651daf6d44348c8cee4
Matt <matthew.king@stfc.ac.uk>
2015-07-08 09:15:44 +0100

    refactored main to comply with jenkins console output

--- exercises/matt_king/ex02_oo_basics/src/main.cpp
+++ exercises/matt_king/ex02_oo_basics/src/main.cpp
@@ -1,5 +1,5 @@
 /**
-* Skeleton main routine
+* main routine
 */
 #include <iostream>
 #include <vector>
@@ -11,13 +11,13 @@
 #include "Square.h"
 #include "Triangle.h"
 
-int main(int, char **)
+std::vector<Shape> creatShapesAndInsertIntoVector()
 {
 	//making shapes
-	Circle circ(6);
-	Square square(7);
-	Rectangle rect(23.3, 3.2);
-	Triangle tri(5, 7);
+	const Circle circ(6);
+	const Square square(7);
+	const Rectangle rect(23.3, 3.2);
+	const Triangle tri(5, 7);
 	
 	std::vector<Shape> shapes;
 
@@ -26,16 +26,20 @@ int main(int, char **)
 	shapes.push_back(rect);
 	shapes.push_back(square);
 	shapes.push_back(tri);
+}
 
-	//create a shapeSorter and feed it shapes
-	ShapeSorter sorter(shapes);
-
+void sortShapes(const ShapeSorter& sorter)
+{
 	//call desired functions of sorter
 	sorter.printShapesThatMatchType("Triangle");
 	sorter.printAreasDesc();
 	sorter.printPerimeterDesc();
 	sorter.printShapesThatMatchNumberOfSides(8);
-	
-	
-	
+}
+
+int main(int, char **)
+{
+	//create a shapeSorter and feed it a populated shape vector
+	ShapeSorter sorter(creatShapesAndInsertIntoVector());
+	sortShapes(sorter);	
 }
