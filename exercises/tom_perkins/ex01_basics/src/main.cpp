59ecb98c9bac380c942eb871120da7d0a594964a
Tom Perkins <thomas.perkins@stfc.ac.uk>
2015-10-28 15:08:19 +0000

    WordCounter: accept filename as an argument
    WordCounter is a command-line program that takes the filename of a text
    file as an argument.
    If no file is specified, a usage message is displayed.

--- exercises/tom_perkins/ex01_basics/src/main.cpp
+++ exercises/tom_perkins/ex01_basics/src/main.cpp
@@ -1,7 +1,20 @@
-/**
- * Skeleton main routine
- */
+#include <iostream>
 
-int main(int, char **)
+/**
+ * Main routine for WordCounter
+ * Get filename from arguments and create a WordCounter to count words
+ */
+int main(int argc, char ** argv)
 {
+	// parse command-line arguments
+	if (argc == 2)
+	{
+		std::cout << "Reading " << argv[1] << "..." << std::endl;
+	}
+	else
+	{
+		std::cout << "Usage: WordCounter.exe <filename>" << std::endl;
+	}
+
+	return 0;
 }
