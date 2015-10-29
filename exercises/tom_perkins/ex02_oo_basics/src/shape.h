16ee57bfde56e61f9150888d9e59d390133760fe
Tom Perkins <thomas.perkins@stfc.ac.uk>
2015-10-29 11:32:12 +0000

    Shapes: abstract base class
    Shape is an abstract base class representing a shape

---
+++ exercises/tom_perkins/ex02_oo_basics/src/shape.h
@@ -0,0 +1,27 @@
+#ifndef SHAPE_H_
+#define SHAPE_H_
+
+//----------------------------------------------------------------------
+// Includes
+//----------------------------------------------------------------------
+#include <string>
+
+/**
+ * \brief Shape is an abstract base class representing a shape.
+ * 
+ * It knows how many sides it has, its type and can
+ * calculate its perimeter and area.
+ */
+class Shape
+{
+public:
+	virtual double perimeter() const = 0; 
+	virtual double area() const = 0;
+
+protected:
+	const int sides;
+	const std::string type; 
+	Shape(int sidesIn, std::string typeIn) : sides(sidesIn), type(typeIn) { };
+};
+
+#endif
