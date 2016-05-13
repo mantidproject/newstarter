e7a7b08bfcb4be376f5e907520dfecf6d8d5fa24
Gagik Vardanyan <vardanyan@ill.fr>
2016-05-13 11:48:22 +0200

    doxygen comments

--- exercises/gagik_vardanyan/ex01_basics/src/main.cpp
+++ exercises/gagik_vardanyan/ex01_basics/src/main.cpp
@@ -1,6 +1,9 @@
-/**
- * Skeleton main routine
+/*!
+ * A main program that reads a text file as command line argument
+ * and creates another file containing the number of occurencies of
+ * distinc words longer then 4 characters in the input file.
  */
+
 #include <algorithm>
 #include <fstream>
 #include <iostream>
@@ -10,7 +13,11 @@
 
 typedef std::vector<std::pair<std::string, int> > pair_vec;
 
-//checks if the character is among allowed punctuation characters
+/**
+* Checks if the given character is among the allowed punctuation characters
+* @param c The char to be tested
+* @return True if c is not in the list, False otherwise
+*/
 bool isNotSpecialCharacter(const char & c)
 {
     static const std::string specialCharacters = ".,?'\"!():";
@@ -18,7 +25,11 @@ bool isNotSpecialCharacter(const char & c)
            == specialCharacters.end();
 }
 
-//builds a vector of pairs out of map
+/**
+* Converts a map to vector of pairs
+* @param map The input map to convert
+* @return The vector of pairs containing the same info as in map
+*/
 pair_vec MapToPairVector(const std::map<std::string,int> & map)
 {
     pair_vec ret;
@@ -32,12 +43,20 @@ pair_vec MapToPairVector(const std::map<std::string,int> & map)
     return ret;
 }
 
-//compares pairs by the value
+/**
+* Compares two pairs by value
+* @param p1 first pair
+* @param p2 second pair
+* @return True if the value of second pair is larger, False otherwise
+*/
 bool CompareCount(const std::pair<std::string, int> & p1, const std::pair<std::string, int> & p2)
 {
     return p1.second < p2.second;
 }
 
+/*!
+ * The main program
+ */
 int main (int argc, char ** argv)
 {
     std::ifstream infile;
@@ -46,7 +65,7 @@ int main (int argc, char ** argv)
     std::map<std::string,int> wordCounts;
     std::string::size_type maxlength = 0;
 
-    //check if the input file name is specified
+    ///check if the input file name is specified
     if (argc < 2)
     {
         std::cout << "Please specify a valid input file!\n";
@@ -55,26 +74,26 @@ int main (int argc, char ** argv)
 
     infile.open(argv[1],std::ios::in);
 
-    //read in word by word
+    ///read in word by word
     while (infile >> word) {
 
-       //erase special characters from word
+       ///erase special characters from word
        std::string::iterator iter =
        std::stable_partition(word.begin(),word.end(),isNotSpecialCharacter);
 
        word.erase(iter,word.end());
 
-       //check the word length
+       ///check the word length
        if(word.size() <= 4)
            continue;
 
-       //transform to lowercase
+       ///transform to lowercase
        std::transform(word.begin(), word.end(), word.begin(), ::tolower);
 
-       //increment the count in the map
+       ///increment the count in the map
        ++wordCounts[word];
 
-       //compute the maxlength for alignment
+       ///compute the maxlength for alignment
        if(word.size() > maxlength)
            maxlength = word.size();
 
@@ -82,7 +101,7 @@ int main (int argc, char ** argv)
 
     infile.close();
 
-    //sort the map by value
+    ///sort the map by value
     pair_vec counts = MapToPairVector(wordCounts);
 
     sort(counts.begin(),counts.end(),CompareCount);
@@ -91,7 +110,7 @@ int main (int argc, char ** argv)
 
     outfile << "Word" << std::string(maxlength-4,' ') << "\t" << "Usage\n\n";
 
-    //print out the results
+    ///print out the results
     for(pair_vec::const_reverse_iterator it = counts.rbegin();
         it != counts.rend(); ++it)
     {
