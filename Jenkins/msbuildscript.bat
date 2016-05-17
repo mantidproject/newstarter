4cf7a8f3314a567725f59da01b1f587784e40327
Owen Arnold <owen.arnold@stfc.ac.uk>
2016-05-17 17:35:39 +0100

    make more readable

--- Jenkins/msbuildscript.bat
+++ Jenkins/msbuildscript.bat
@@ -3,7 +3,8 @@
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 set VS_VERSION=14
 call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" amd64
-@echo $3
-@echo $2
-@echo $1
-%3 %1 %2
+:: %1 is the full path to the msbuild executable
+:: %2 is the configuration Release/Debug etc
+:: %3 is the full path to the solution
+
+%1 %2 %3
