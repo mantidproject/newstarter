b760a055ab84f918576817a8ab30272da87953f0
Owen Arnold <owen.arnold@stfc.ac.uk>
2016-05-17 17:40:18 +0100

    put comments together

--- Jenkins/msbuildscript.bat
+++ Jenkins/msbuildscript.bat
@@ -1,10 +1,10 @@
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Environment setup
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-set VS_VERSION=14
-call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" amd64
 :: %1 is the full path to the msbuild executable
 :: %2 is the configuration Release/Debug etc
 :: %3 is the full path to the solution
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+set VS_VERSION=14
+call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" amd64
 %1 %2 %3
