c29754b19d37fb65b74ebf823ee2a7dc09cbd235
Owen Arnold <owen.arnold@stfc.ac.uk>
2016-05-17 18:15:24 +0100

    Path gets too long

--- Jenkins/msbuildscript.bat
+++ Jenkins/msbuildscript.bat
@@ -1,10 +1,13 @@
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Environment setup
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+setlocal enableextensions enabledelayedexpansion
+set VS_VERSION=14
+if not defined DevEnvDir (
+    call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" amd64
+)
 :: %1 is the full path to the msbuild executable
 :: %2 is the configuration Release/Debug etc
 :: %3 is the full path to the solution
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-set VS_VERSION=14
-call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" amd64
 %1 %2 %3
