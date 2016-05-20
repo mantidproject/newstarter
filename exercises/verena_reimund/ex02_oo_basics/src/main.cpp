a058f8dfe453628efb6bbdf75fcbdd36502d9f58
reimundILL <reimundILL@users.noreply.github.com>
2016-05-20 09:07:22 +0200

    exercise 2: cleanup variable names, removed base class print, base class shape is now abstract with pure virtual member functions, added compare_sides for comparing integer values if identical, cleaned member functions of shapesorter

--- exercises/verena_reimund/ex02_oo_basics/src/main.cpp
+++ exercises/verena_reimund/ex02_oo_basics/src/main.cpp
@@ -1,181 +1,159 @@
 /**
-* New type: Shapes (circle, ..) with functionality defined in class ShapeSorter
+* New type: shape (circle, ..) with functionality defined in class shapeorter
 */
-#include <math.h>    //pow
+#include <math.h>    //pow, M_PI
 #include <iostream>
 #include <vector>
 #include <string>
 #include <algorithm> //for_each
 //#include <memory>  //smart pointers
 
-
-// base class
-class print{
-public:// overloaded print functions
-   void out (std::string type, double val);
-   void out (std::string type, std::string val);
-   void out (std::string type, double val1, double val2);
-};
-
 // abstract base class
-class Shapes: public print{
-   std::string n;
-   double shape_area;
-   double shape_perimeter;
-   std::string n_sides;
+class shape{
 public:
-   Shapes(){}
-   virtual std::string type() const {return n;}
-   virtual std::string Square() const {return n_sides;}
-   virtual double perimeter () const {return shape_perimeter;}
-   virtual double area () const {return shape_area;}
-   virtual ~Shapes(){}
+    shape(){}
+    virtual std::string type() const =0;
+    virtual int number_of_sides() const =0;
+    virtual double perimeter () const =0;
+    virtual double area () const =0;
+    virtual ~shape(){}
 };
 
 // type definitions
 
-typedef std::vector<Shapes*> collection; // std::unique_ptr
-typedef std::vector<Shapes*>::size_type its;
+typedef std::vector<shape*> collection; // std::unique_ptr
+typedef std::vector<shape*>::size_type its;
 
 // compare functions, const required
 bool compare_strings (const std::string &s1, const std::string &s2){
-   return s1.compare(s2)==0;
+    return s1.compare(s2)==0;
 }
 
-bool compare_volume (const Shapes *s1, const Shapes *s2){
-   return s1->area()>s2->area();
+bool compare_volume (const shape *s1, const shape *s2){
+    return s1->area()>s2->area();
 }
 
-bool compare_perimeter (const Shapes *s1, const Shapes *s2){
-   return s1->perimeter()>s2->perimeter();
+bool compare_perimeter (const shape *s1, const shape *s2){
+    return s1->perimeter()>s2->perimeter();
+}
+
+bool compare_sides (const int &s1, const int &s2){
+    return s1==s2;
 }
 
 // class definitions
 
-class rectangle: public Shapes{
-   double side1;
-   double side2;
+class rectangle: public shape{
+    double side1;
+    double side2;
 public:
-   rectangle(double x, double y) : side1(x), side2(y) {} // constructor
-   rectangle(double z) : side1(z), side2(z) {} // constructor squares
-   std::string type() const {return "Rectangle";}
-   std::string Square() const {return "4";}
-   double perimeter() const {return (2*(side1+side2));}
-   double area() const {return (side1*side2);}
+    rectangle(double x, double y) : side1(x), side2(y) {} // constructor
+    rectangle(double z) : side1(z), side2(z) {} // constructor number_of_sides
+    std::string type() const {return "Rectangle";}
+    int number_of_sides() const {return 4;}
+    double perimeter() const {return (2*(side1+side2));}
+    double area() const {return (side1*side2);}
 };
 
-class circle: public Shapes{
-   double radius;
+class circle: public shape{
+    double radius;
 public:
-   explicit circle(double r) : radius(r) {} // constructor, default constructor disabled
-   std::string type() const {return "Circle";}
-   std::string Square() const {return "inf";}
-   double perimeter() const {return(2*M_PI*radius);}
-   double area() const {return (M_PI*pow(radius,2));}
+    explicit circle(double r) : radius(r) {} // constructor, default constructor disabled
+    std::string type() const {return "Circle";}
+    int number_of_sides() const {return -1;} // infinity, not an unsigned int
+    double perimeter() const {return(2*M_PI*radius);}
+    double area() const {return (M_PI*pow(radius,2));}
 };
 
-class triangle: public Shapes{
-   double height;
-   double base;
+class triangle: public shape{
+    double height;
+    double base;
 public:
-   triangle(double h, double b) : height(h), base(b) {}
-   std::string type() const {return "Triangle";}
-   std::string Square() const {return "3";}
-   double perimeter() const {return(base+2*sqrt(pow(height,2.0)+pow(base,2.0)/4));}
-   double area() const {return (height*base/2);}
+    triangle(double h, double b) : height(h), base(b) {}
+    std::string type() const {return "Triangle";}
+    int number_of_sides() const {return 3;}
+    double perimeter() const {return(base+2*sqrt(pow(height,2.0)+pow(base,2.0)/4));}
+    double area() const {return (height*base/2);}
 };
 
-class ShapeSorter: public Shapes{
-   collection shapes;
+class shapesorter{
+    collection shape;
 public:
-   explicit ShapeSorter(collection s) : shapes(s) {}
-   void type(std::string select_type);
-   void sides(std::string select_number_of_sides);
-   void volume_dec();
-   void perimeter_dec();
+    explicit shapesorter(collection s) : shape(s) {}
+    void type(std::string select_type);
+    void sides(int select_number_of_sides);
+    void volume_dec();
+    void perimeter_dec();
 };
 
 // member functions
 
-void print::out(std::string type, double val) {
-   std::cout << type << "\t" << val << std::endl;
+void out(std::string type, double val) {
+    std::cout << type << "\t" << val << std::endl;
 }
 
-void print::out(std::string type, double val1, double val2) {
-   std::cout << type << "\t" << val1 << "\t" << val2 << std::endl;
+void out(std::string type, double val1, double val2) {
+    std::cout << type << "\t" << val1 << "\t" << val2 << std::endl;
 }
 
-void print::out(std::string type, std::string val) {
-   std::cout << type << "\t" << val << std::endl;
-}
 
 // would like to print values of derived class members like side1, side2, .. using dynamic_cast
 
-void ShapeSorter::type(std::string select_type){
-  std::cout << "Shapes of type : " << select_type << std::endl;
-  for(its i = 0; i!=shapes.size(); ++i){
-    if (compare_strings(select_type,shapes[i]->type())){
-      shapes[i]->out(shapes[i]->type(),shapes[i]->area());
+void shapesorter::type(std::string select_type){
+    std::cout << "shape of type : " << select_type << std::endl;
+    for(its i = 0; i!=shape.size(); ++i){
+        if (compare_strings(select_type,shape[i]->type())){
+            out(shape[i]->type(),shape[i]->area());
+        }
     }
-  }
-  std::cout << '\n' << std::endl;
+    std::cout << '\n' << std::endl;
 }
 
-void ShapeSorter::sides(std::string number_of_sides){
-  std::cout << "Shapes with number of sides : " << number_of_sides << std::endl;
-  for(its i = 0; i!=shapes.size(); ++i){
-    if (compare_strings(number_of_sides,shapes[i]->Square())){
-      shapes[i]->out(shapes[i]->type(),shapes[i]->Square());
+void shapesorter::sides(int number_of_sides){
+    std::cout << "shape with number of sides : " << number_of_sides << std::endl;
+    for(its i = 0; i!=shape.size(); ++i){
+        if (compare_sides(number_of_sides,shape[i]->number_of_sides())){
+            out(shape[i]->type(),shape[i]->number_of_sides());
+        }
     }
-  }
 }
 
-void ShapeSorter::volume_dec(){
-  std::cout << "Volume of shapes in decreasing order : " << std::endl;
-  sort(shapes.begin(),shapes.end(),compare_volume);
-  for(its i = 0; i!=shapes.size(); ++i){
-    shapes[i]->out(shapes[i]->type(),shapes[i]->area());
-  }
-  std::cout << '\n' << std::endl;
+void shapesorter::volume_dec(){
+    sort(shape.begin(),shape.end(),compare_volume);
 }
 
-void ShapeSorter::perimeter_dec(){
-  std::cout << "Perimeter of shapes in decreasing order : " << std::endl;
-  sort(shapes.begin(),shapes.end(),compare_perimeter);
-  for(its i = 0; i!=shapes.size(); ++i){
-    shapes[i]->out(shapes[i]->type(),shapes[i]->perimeter());
-  }
-  std::cout << '\n' << std::endl;
+void shapesorter::perimeter_dec(){
+    sort(shape.begin(),shape.end(),compare_perimeter);
 }
 
 // template function in order to delete classes
 template <typename T>
-void delete_shapes(T* const ptr){delete ptr;}
+void delete_shape(T* const ptr){delete ptr;}
 
 int main(int argc, char** argv){
-  try{
+    try{
 
-   collection cshapes;
+        collection cshape;
 
-   cshapes.push_back(new rectangle(4.5,2.0));// std::make_unique
-   cshapes.push_back(new rectangle(1.5,2.0));
-   cshapes.push_back(new circle(3.0));
-   cshapes.push_back(new rectangle(4.0));
-   cshapes.push_back(new triangle(2.0,2.0));
+        cshape.push_back(new rectangle(4.5,2.0));// std::make_unique
+        cshape.push_back(new rectangle(1.5,2.0));
+        cshape.push_back(new circle(3.0));
+        cshape.push_back(new rectangle(4.0));
+        cshape.push_back(new triangle(2.0,2.0));
 
-   ShapeSorter shape_sorter(cshapes);
-   shape_sorter.type("Rectangle");
-   shape_sorter.sides("4");
-   shape_sorter.volume_dec();
-   shape_sorter.perimeter_dec();
+        shapesorter csorter(cshape);
+        csorter.type("Rectangle");
+        csorter.sides(4);
+        csorter.volume_dec();
+        csorter.perimeter_dec();
 
-   std::for_each(cshapes.begin(), cshapes.end(), delete_shapes<Shapes>); // required deletes
-   return 0;
-    
-  }
-  catch (std::exception const& ex) {//catch standard exception
-    std::cout << "Exception: " << ex.what() <<std::endl;
-    return -1;
-  }
+        std::for_each(cshape.begin(), cshape.end(), delete_shape<shape>); // deletes
+        return 0;
+
+    }
+    catch (std::exception const& ex) {//catch standard exception
+        std::cout << "Exception: " << ex.what() <<std::endl;
+        return -1;
+    }
 
 }
