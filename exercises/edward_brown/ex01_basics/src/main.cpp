2bb5283a0784c416bdbf06ff29363d733fba088c
Edward Brown <edward.brown@stfc.ac.uk>
2017-07-06 11:50:09 +0100

    Added basic file io to main.cpp

--- exercises/edward_brown/ex01_basics/src/main.cpp
+++ exercises/edward_brown/ex01_basics/src/main.cpp
@@ -1,7 +1,41 @@
-/**
- * Skeleton main routine
- */
+#include <ifstream>
+#include <ofstream>
+#include <string>
 
-int main(int, char **)
-{
+std::ifstream openInputFile(std::string const &filename) {
+  auto InputFile = std::ifstream(filename);
+  if (InputFile.is_open()) {
+    return InputFile;
+  } else {
+    throw std::runtime_error(
+        "Input file does not exist or has incorect permisssions.");
+  }
+}
+
+std::ofstream openOutputFile(std::string const &filename) {
+  auto OutputFile = std::ofstream("results.txt", std::ofstream::out);
+  if (OutputFile.is_open()) {
+    return OutputFile;
+  } else {
+    throw std::runtime_error(
+        "Failed to create an output file, check directory permissions.");
+  }
+}
+
+std::string fileToString(std::ifstream& file) {
+  auto FileStream = std::stringstream();
+  FileStream << file.rdbuf();
+  return FileStream.str();
+}
+
+int main(int argc, char **argv) {
+  if (argc > 1) {
+    try {
+      auto const InputFileName = std::string(argv[0]);
+      auto InputFileStream = openInputFile(InputFileName);
+      auto OutputFileStream = openOutputFile();
+    } catch (std::runtime_exception const &ex) {
+      std::cout << ex.what() << std::endl;
+    }
+  }
 }
