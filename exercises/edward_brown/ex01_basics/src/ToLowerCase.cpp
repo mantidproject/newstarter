d55a93efdffa896255833269ad01d08ddc2f5656
Edward Brown <edward.brown@stfc.ac.uk>
2017-07-07 09:10:27 +0100

    Added missing include for std::tolower in ToLowerCase.

--- exercises/edward_brown/ex01_basics/src/ToLowerCase.cpp
+++ exercises/edward_brown/ex01_basics/src/ToLowerCase.cpp
@@ -1,5 +1,7 @@
 #include "ToLowerCase.h"
 #include <algorithm>
+#include <byte>
+
 std::string toLowerCase(std::string MixedOrUpperCase) {
   std::transform(MixedOrUpperCase.cbegin(), MixedOrUpperCase.cend(),
                  MixedOrUpperCase.begin(),
