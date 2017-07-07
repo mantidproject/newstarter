145fe40f843868ab4bdff1abe38954b7863ece7e
Edward Brown <edward.brown@stfc.ac.uk>
2017-07-07 10:40:01 +0100

    Added comments to helpers in main.

--- exercises/edward_brown/ex01_basics/src/main.cpp
+++ exercises/edward_brown/ex01_basics/src/main.cpp
@@ -6,16 +6,23 @@
 #include <stdexcept>
 #include <vector>
 
+/** Ensures the existance of at least one command line argument to be used as the input
+ *  file name.
+ *  @param ArgumentCount The value of the argc parameter passed to main.
+ *  @return True if the minimum number of arguments or greater are present.
+ */
 bool hasRequiredCommandLineArguments(int ArgumentCount) {
-  auto constexpr MinimumCommandLineArguments = 1;
-  return MinimumCommandLineArguments <= ArgumentCount;
+  auto constexpr MINIMUM_COMMAND_LINE_ARGUMENTS = 1;
+  return MINIMUM_COMMAND_LINE_ARGUMENTS <= ArgumentCount;
 }
 
+/// Indicates to the user that the output has been created and written successfully.
 void indicateSuccess() {
   std::cout << "Successfully wrote word frequencies to results.txt."
             << std::endl;
 }
 
+/// Indicates to the user that no input files were provided.
 void noInputFiles() { std::cerr << "No input file specified." << std::endl; }
 
 int main(int argc, char **argv) {
