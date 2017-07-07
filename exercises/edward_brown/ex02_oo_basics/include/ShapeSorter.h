afa2e760e72e85d8d3980d6debcf3b53182c3ef6
Edward Brown <edward.brown@stfc.ac.uk>
2017-07-07 16:21:49 +0100

    Started writing shapesorter.

--- exercises/edward_brown/ex02_oo_basics/include/ShapeSorter.h
+++ exercises/edward_brown/ex02_oo_basics/include/ShapeSorter.h
@@ -1,8 +1,31 @@
 #ifndef EX2_SHAPE_SORTER
 #define EX2_SHAPE_SORTER
+#include "Shape.h"
+
 class ShapeSorter {
   public:
+    ShapeSorter(std::vector<std::unique_ptr<Shape>> Shapes);
+    void printOfType(std::string Type) const;
+    void printWithSides(int N) const;
+    void printVolumeDescending() const;
+    void printPerimeterdDescending() const;
+  private:
+    template <typename ShapePredicate>
+    void printWhere(ShapePredicate ShouldPrint) const {
+      for(auto&& Shape : m_Shapes) {
+        if(ShouldPrint(Shape)) {
+          std::cout << Shape << '\n';
+        }
+        std::cout << std::endl;
+      }
+    }
 
+    template <typename ComparisonFunction>
+    void printOrderedBy(ComparisonFunction Compare) const {
+
+    }
+
+    std::vector<std::unique_ptr<Shape>> m_Shapes;
 };
 
 
