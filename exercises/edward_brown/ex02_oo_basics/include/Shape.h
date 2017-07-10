e75b745c2fadaf0f54e61ff51359a0c3d55ec09f
Edward Brown <edward.brown@stfc.ac.uk>
2017-07-10 09:27:03 +0100

    Added documentation and minor code tidying, remove use of const ptr where a reference will do.

--- exercises/edward_brown/ex02_oo_basics/include/Shape.h
+++ exercises/edward_brown/ex02_oo_basics/include/Shape.h
@@ -1,6 +1,5 @@
 #ifndef EX2_SHAPE
 #define EX2_SHAPE
-#include <ostream>
 #include <string>
 class Shape {
   friend std::ostream& operator<<(std::ostream& OutputStream, Shape const& ToPrint);
@@ -11,6 +10,10 @@ class Shape {
     virtual double getArea() const = 0;
     virtual ~Shape() = default;
   private:
+    /** Prints the names and values of the attributes of this shape.
+     * Properties are printed in the following format.
+     * <AttributeName1>: <AttributeValue1>[, <AttributeNameN>: <AttributeValueN> ...]
+     */
     virtual void printAttributes(std::ostream& OutputStream) const = 0;
 };
 #endif // EX2_SHAPE
--- exercises/edward_brown/ex02_oo_basics/include/ShapeSorter.h
+++ exercises/edward_brown/ex02_oo_basics/include/ShapeSorter.h
@@ -1,50 +1,59 @@
 #ifndef EX2_SHAPE_SORTER
 #define EX2_SHAPE_SORTER
+#include "Shape.h"
 #include <algorithm>
 #include <memory>
 #include <ostream>
 #include <vector>
-#include "Shape.h"
 
 class ShapeSorter {
-   public:
-    using UniqueShape = std::unique_ptr<Shape>;
-    using ShapeVector = std::vector<UniqueShape>;
-    ShapeSorter(ShapeVector Shapes, std::ostream& OutputStream);
-    void printOfType(std::string Type);
-    void printWithSides(int N);
-    void printByAreaDescending();
-    void printByPerimeterDescending();
+public:
+  using UniqueShape = std::unique_ptr<Shape>;
+  using ShapeVector = std::vector<UniqueShape>;
+  ShapeSorter(ShapeVector Shapes, std::ostream &OutputStream);
+  void printOfType(std::string Type);
+  void printWithSides(int N);
+  void printByAreaDescending();
+  void printByPerimeterDescending();
 
-   private:
-    void printShape(Shape const* const Shape);
-    template <typename ShapePredicate>
-    void printWhere(ShapePredicate ShouldPrint);
-    template <typename ComparisonFunction>
-    void printOrderedBy(ComparisonFunction Compare);
-    void printAll();
-    ShapeVector m_Shapes;
-    std::ostream& m_OutputStream;
+private:
+  void printShape(Shape const& Shape);
+  template <typename ShapePredicate>
+  void printWhere(ShapePredicate ShouldPrint);
+  template <typename ComparisonFunction>
+  void printOrderedBy(ComparisonFunction Compare);
+  void printAll();
+  ShapeVector m_Shapes;
+  std::ostream &m_OutputStream;
 };
 
+/** Prints all shapes s for which ShapePredicate(s) returns an object which is
+ * contextually convertible to true.
+ * @param ShouldPrint Predicate which defines the set of shapes to print.
+ */
 template <typename ShapePredicate>
 void ShapeSorter::printWhere(ShapePredicate ShouldPrint) {
-    std::for_each(m_Shapes.cbegin(), m_Shapes.cend(),
-                  [this, &ShouldPrint](UniqueShape const& Shape) -> void {
-                    if(ShouldPrint(*Shape)) {
-                      printShape(Shape.get());
-                    }
-                  });
-    m_OutputStream << std::endl;
+  std::for_each(m_Shapes.cbegin(), m_Shapes.cend(),
+                [this, &ShouldPrint](UniqueShape const &Shape) -> void {
+                  if (ShouldPrint(*Shape)) {
+                    printShape(*Shape);
+                  }
+                });
+  m_OutputStream << std::endl;
 }
 
+/** Prints all the shapes in the order determined by the comparison function
+ * Compare.
+ * @param Compare A fuction accepting two shapes which returns true if they are
+ * ordered correctly, false if they are not.
+ */
 template <typename ComparisonFunction>
 void ShapeSorter::printOrderedBy(ComparisonFunction Compare) {
-    std::sort(m_Shapes.begin(), m_Shapes.end(),
-              [&Compare](UniqueShape const& First,
-                         UniqueShape const& Last) -> bool {
-                  return Compare(*First, *Last);
-              });
-    printAll();
+  std::sort(
+      m_Shapes.begin(), m_Shapes.end(),
+      [&Compare](UniqueShape const &First, UniqueShape const &Last) -> bool {
+        return Compare(*First, *Last);
+      });
+  printAll();
 }
-#endif  // EX2_SHAPE_SORTER
+#endif // EX2_SHAPE_SORTER
