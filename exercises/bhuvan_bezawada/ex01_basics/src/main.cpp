4617181424577e67791e70517fb881936a1ff01f
Bhuvan Bezawada <bhuvan_777@outlook.com>
2018-06-06 16:28:37 +0100

    Fixed indentation and style issues

--- exercises/bhuvan_bezawada/ex01_basics/src/main.cpp
+++ exercises/bhuvan_bezawada/ex01_basics/src/main.cpp
@@ -1,150 +1,151 @@
 /**
  * PROGRAM DESCRIPTION:
- * 
- * A command line program to read in a text file and give the occurrences of the words. 
  *
-**/
+ * A command line program to read in a text file and give the occurrences of the
+ *words.
+ *
+ **/
 
 // Imports
-#include <iostream>
-#include <string>
-#include <map>
-#include <vector>
-#include <fstream>
 #include <algorithm>
+#include <fstream>
+#include <iostream>
 #include <iterator>
+#include <map>
+#include <string>
+#include <vector>
 
 // Namespace being used
 using namespace std;
 
-
 /**
  * Code to flip the <string, int> pairs.
- * Code adapted from: https://stackoverflow.com/questions/5056645/sorting-stdmap-using-value?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-**/
-template<typename A, typename B>
-pair<B, A> flip_pair(const pair<A, B> &p) {
-    return pair<B, A>(p.second, p.first);
+ * Code adapted from:
+ *https://stackoverflow.com/questions/5056645/sorting-stdmap-using-value?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+ **/
+template <typename A, typename B> pair<B, A> flipPair(const pair<A, B> &p) {
+  return pair<B, A>(p.second, p.first);
 }
 
-template<typename A, typename B>
-multimap<B, A> flip_map(const map<A, B> &src) {
-    multimap<B, A> dst;
-    transform(src.begin(), src.end(), inserter(dst, dst.begin()), flip_pair<A, B>);
-    return dst;
+template <typename A, typename B> multimap<B, A> flipMap(const map<A, B> &src) {
+  multimap<B, A> dst;
+  transform(src.begin(), src.end(), inserter(dst, dst.begin()), flipPair<A, B>);
+  return dst;
 }
-
 
 /**
  *  Split function to take a line of input and return words in line.
  *  From Accelerated C++ Textbook, page 88.
-**/
-vector<string> split(const string& s) {
-    // Create return object
-    vector<string> ret;
-    typedef string::size_type string_size;
-    string_size i = 0;
+ **/
+vector<string> split(const string &s) {
+  // Create return object
+  vector<string> ret;
+  typedef string::size_type string_size;
+  string_size i = 0;
 
-    // Process characters
-    while(i != s.size()) {
-        // Ignore the leading blanks
-        while(i != s.size() && isspace(s[i])) {
-            ++i;
-        }
-
-        // Find end of next word
-        string_size j = i;
-        while(j != s.size() && !isspace(s[j])) {
-            ++j;
-        }
-        
-        // Found some non-whitespace characters
-        if(i != j) {
-            ret.push_back(s.substr(i, j-i));
-            i = j;
-        }
+  // Process characters
+  while (i != s.size()) {
+    // Ignore the leading blanks
+    while (i != s.size() && isspace(s[i])) {
+      ++i;
     }
 
-    return ret;
-}
+    // Find end of next word
+    string_size j = i;
+    while (j != s.size() && !isspace(s[j])) {
+      ++j;
+    }
 
+    // Found some non-whitespace characters
+    if (i != j) {
+      ret.push_back(s.substr(i, j - i));
+      i = j;
+    }
+  }
+
+  return ret;
+}
 
 /**
  * Main function.
-**/
+ **/
 int main() {
-    // Get file name from user and store
-    cout << "Please enter the name of the file: " << endl;
-    string file_name;
-    cin >> file_name;
-	ifstream input_file(file_name);
-	
-	// Try to load the file
-	if(!input_file) {
-		cerr << "Error, there was a problem opening/reading the file." << endl;
-		exit(EXIT_FAILURE);
-	}
+  // Get file name from user and store
+  cout << "Please enter the name of the file: " << endl;
+  string fileName;
+  cin >> fileName;
+  ifstream inputFile(fileName);
 
-	// Check if file is empty
-	// Adapted from https://stackoverflow.com/questions/2390912/checking-for-an-empty-file-in-c
-	if(input_file.peek() == std::ifstream::traits_type::eof()) {
-		cerr << "Error, the file was empty!" << endl;
-	}
+  // Try to load the file
+  if (!inputFile) {
+    cerr << "Error, there was a problem opening/reading the file." << endl;
+    return -1;
+  }
 
-    // To store line of text and words and their counts
-    string line;
-    map<string, int> word_and_count;
+  // Check if file is empty
+  // Adapted from
+  // https://stackoverflow.com/questions/2390912/checking-for-an-empty-file-in-c
+  if (inputFile.peek() == std::ifstream::traits_type::eof()) {
+    cerr << "Error, the file was empty!" << endl;
+    return -1;
+  }
 
-    // Loop through each line
-    while(getline(input_file, line)) {
+  // To store line of text and words and their counts
+  string line;
+  map<string, int> wordAndCount;
 
-        // Get the list of words in each line
-        vector<string> words = split(line);
+  // Loop through each line
+  while (getline(inputFile, line)) {
 
-        // Process each word
-        for (vector<string>::const_iterator it = words.begin(); it != words.end(); it++) {
-            // Dereference the iterator to get back the word
-            string word = *it;
+    // Get the list of words in each line
+    vector<string> words = split(line);
 
-            // Loop through the word
-            for(int i = 0; i < word.size(); i++) {
-                // Deal with hyphens?
-                // Deal with allowed punctuation?
-                
-                // Remove all punctuation and white space
-                // Convert upper case letters to lowercase letters
-                if(ispunct(word[i])) {
-                    word.erase(i--, 1);
-                } else if(isupper(word[i])) {
-                    word[i] = tolower(word[i]);
-                }
-            }
+    // Process each word
+    for (vector<string>::const_iterator it = words.begin(); it != words.end();
+         it++) {
+      // Dereference the iterator to get back the word
+      string word = *it;
 
-            // Only count words that are longer than 4 letters
-            if(word.length() > 4) {
-                ++word_and_count[word];
-            }
+      // Loop through the word
+      for (int i = 0; i < word.size(); i++) {
+        // Deal with hyphens?
+        // Deal with allowed punctuation?
+
+        // Remove all punctuation and white space
+        // Convert upper case letters to lowercase letters
+        if (ispunct(word[i])) {
+          word.erase(i--, 1);
+        } else if (isupper(word[i])) {
+          word[i] = tolower(word[i]);
         }
+      }
+
+      // Only count words that are longer than 4 letters
+      if (word.length() > 4) {
+        ++wordAndCount[word];
+      }
     }
+  }
 
-	cout << "END..." << endl;
+  // Header for output
+  cout << "Word"
+       << "\t\t"
+       << "Usage" << endl;
+  cout << "" << endl;
 
-    // Header for output
-    cout << "Word" << "\t\t" << "Usage" << endl;
-    cout << "" << endl;
+  // Flip the output
+  multimap<int, string> dst = flipMap(wordAndCount);
 
-    // Flip the output
-    multimap<int, string> dst = flip_map(word_and_count);
+  // Print out the word and its count (to terminal and file)
+  ofstream outputFile;
+  outputFile.open("results.txt");
+  for (multimap<int, string>::const_reverse_iterator it = dst.rbegin();
+       it != dst.rend(); ++it) {
+    cout << it->second << "\t\t" << it->first << endl;
+    outputFile << it->second << "\t\t" << it->first << endl;
+  }
+  cout << "Finished writing results to 'results.txt'..." << endl;
+  outputFile.close();
 
-    // Print out the word and its count (to terminal and file)
-    ofstream out_file;
-    out_file.open("results.txt");
-    for(multimap <int, string>::const_reverse_iterator it = dst.rbegin(); it != dst.rend(); ++it) {
-        cout << it->second << "\t\t" << it->first << endl;
-        out_file << it->second << "\t\t" << it->first << endl;
-    }
-    out_file.close();
-
-    return 0;
+  return 0;
 }
-
