256f2c33a9fee0b8fd596be6b1ad3f3136ab77fc
Sam <samjones714@gmail.com>
2018-07-03 14:30:31 +0100

    Clang formatted the code

--- exercises/Samuel_Jones/ex01_basics/main.cpp
+++ exercises/Samuel_Jones/ex01_basics/main.cpp
@@ -12,9 +12,9 @@ using namespace std;
 bool doesFileExist(string &);
 string getFileName();
 void readWords(vector<string> *, ifstream &);
-void findOccurencesofWords(vector<string>, vector< pair<string,int> > *);
+void findOccurencesofWords(vector<string>, vector<pair<string, int>> *);
 bool notDelim(char);
-bool sortingWords(const pair<string,int>&, const pair<string,int>&);
+bool sortingWords(const pair<string, int> &, const pair<string, int> &);
 
 /**
  * Skeleton main routine
@@ -27,7 +27,7 @@ int main(int, char **)
 
     //Load the ascii file
     vector<string> fileStrings;
-    vector< pair<string, int> > uniqueWords;
+    vector<pair<string, int>> uniqueWords;
 
     ifstream fs;
     fs.open(fileName.c_str());
@@ -38,7 +38,7 @@ int main(int, char **)
 
     //Make sure that the unique words vector is in the correct order
     findOccurencesofWords(fileStrings, &uniqueWords);
-    
+
     //Sort the lists based on the number of times it is present
     sort(uniqueWords.begin(), uniqueWords.end(), sortingWords);
 
@@ -47,16 +47,19 @@ int main(int, char **)
     results.open("results.txt");
     results << "Word\t\tUsage" << endl;
     results << endl;
-    for(auto i = 0u; i<uniqueWords.size(); i++){
-        pair<string,int> currentWord = uniqueWords.at(i);
+    for (auto i = 0u; i < uniqueWords.size(); i++)
+    {
+        pair<string, int> currentWord = uniqueWords.at(i);
         results << currentWord.first << "\t\t" << currentWord.second << endl;
     }
 }
 
 //Get the file name a)nd check whether or not the file exists etc.
-string getFileName(){
+string getFileName()
+{
     string fileName;
-    do{
+    do
+    {
         fileName = "";
         cout << "Give the  file name" << endl;
         cin >> fileName;
@@ -65,30 +68,39 @@ string getFileName(){
 }
 
 //Return true if the file exists else false
-bool doesFileExist(string &fileName){
+bool doesFileExist(string &fileName)
+{
     std::ifstream infile(fileName.c_str());
-    if(!infile.good()){
+    if (!infile.good())
+    {
         cout << "File could not be found!" << endl;
     }
     return infile.good();
 }
 
-void readWords(vector<string> *words, ifstream &fs){
+void readWords(vector<string> *words, ifstream &fs)
+{
     //Create the stream buffer for content into a string.
     string contents((istreambuf_iterator<char>(fs)), istreambuf_iterator<char>());
 
     //Create the word utilising the buffer iterators
     string tempWord;
-    for(auto i = 0u; i<contents.length(); i++){
-        if(notDelim(contents[i])){
-            tempWord+=contents[i];
-        } else if(tempWord != "" && tempWord.length() >= 4){
+    for (auto i = 0u; i < contents.length(); i++)
+    {
+        if (notDelim(contents[i]))
+        {
+            tempWord += contents[i];
+        }
+        else if (tempWord != "" && tempWord.length() >= 4)
+        {
             //Make sure word is all lower case
             transform(tempWord.begin(), tempWord.end(), tempWord.begin(), ::tolower);
 
             words->push_back(tempWord);
             tempWord = "";
-        } else{
+        }
+        else
+        {
             tempWord = "";
         }
     }
@@ -97,29 +109,46 @@ void readWords(vector<string> *words, ifstream &fs){
 }
 
 //If the charecter is one of the delimeters return false
-bool notDelim(char c){
-    switch(c){
-        case '.': return false;
-        case ' ': return false;
-        case ',': return false;
-        case '?': return false;
-        case '\'': return false;
-        case '\\': return false;
-        case '"': return false;
-        case '!': return false;
-        case '(': return false;
-        case ')': return false;
-        case ':': return false;
-        case '\n': return false;
-        case '\r': return false;
-        case '-': return false;
-        default:
-            return true;
-            break;
+bool notDelim(char c)
+{
+    switch (c)
+    {
+    case '.':
+        return false;
+    case ' ':
+        return false;
+    case ',':
+        return false;
+    case '?':
+        return false;
+    case '\'':
+        return false;
+    case '\\':
+        return false;
+    case '"':
+        return false;
+    case '!':
+        return false;
+    case '(':
+        return false;
+    case ')':
+        return false;
+    case ':':
+        return false;
+    case '\n':
+        return false;
+    case '\r':
+        return false;
+    case '-':
+        return false;
+    default:
+        return true;
+        break;
     }
 }
 
-void findOccurencesofWords(vector<string> words, vector< pair<string, int> > *uniqueWords){
+void findOccurencesofWords(vector<string> words, vector<pair<string, int>> *uniqueWords)
+{
     string currentWord = "";
     string lastWord;
     bool inSet = false;
@@ -127,23 +156,31 @@ void findOccurencesofWords(vector<string> words, vector< pair<string, int> > *un
     //Make sure that the vector is sorted
     sort(words.begin(), words.end());
 
-    for(auto i = 0u; i<words.size(); i++){
+    for (auto i = 0u; i < words.size(); i++)
+    {
         lastWord = currentWord;
         currentWord = words.at(i);
         //If new word is same as last word then then check if new set
-        if(currentWord == lastWord){
-            if(inSet){
+        if (currentWord == lastWord)
+        {
+            if (inSet)
+            {
                 //Is in a set already and thus we are adding to the tally
                 currentTally++;
-            }else{
+            }
+            else
+            {
                 //Isn't already in a set and so we are creating a new set for the vector of lists of strings
                 inSet = true;
                 currentTally++;
             }
-        } else {
+        }
+        else
+        {
             //There is an issue whether it will accept null charecters and I am filtering them out, at submission
             //This reduces the load on checking all of the passed words.
-            if(lastWord == ""){
+            if (lastWord == "")
+            {
                 continue;
             }
             //Check whether or not the set has ended
@@ -161,6 +198,7 @@ void findOccurencesofWords(vector<string> words, vector< pair<string, int> > *un
     uniqueWords->push_back(newSet);
 }
 
-bool sortingWords (const pair<string,int> &a, const pair<string,int> &b){
+bool sortingWords(const pair<string, int> &a, const pair<string, int> &b)
+{
     return a.second > b.second;
 }
