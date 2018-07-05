399ffa5eeb6ad0b5d7b4b6ddd12ed39df54f754a
Sam Jenkins <s.jenkins@stfc.ac.uk>
2018-07-05 08:34:37 +0100

    updated mapFunctions.h to reflect changes to cpp

--- exercises/sam_jenkins/ex01_basics/src/mapFunctions.h
+++ exercises/sam_jenkins/ex01_basics/src/mapFunctions.h
@@ -6,3 +6,4 @@
 
 std::unordered_map<std::string, int> countof(const std::string& input);
 std::vector<std::pair<std::string, int>> mapToVector(std::unordered_map<std::string, int>map);
+bool compare(const std::pair<std::string, int>& left, const std::pair<std::string, int>& right);
