b199702b026ab5b5cf3c21ae9191cc7b47521d34
AndreBam <ayomide.bamidele@stfc.ac.uk>
2018-07-20 11:27:09 +0100

    adjusted to try and prevent RHEL errors

--- exercises/ayomide_bamidele/ex02_oo_basics/src/ShapeSorter.cpp
+++ exercises/ayomide_bamidele/ex02_oo_basics/src/ShapeSorter.cpp
@@ -6,7 +6,7 @@
 #include <vector>
 
 void ShapeSorter::printByType(const std::vector<Shape *> &shapes,
-                             const std::string &type) {
+                              const std::string &type) {
   for (const auto &shape : shapes) {
     if (shape->getType() == type) {
       std::cout << shape->getType() << " has the right type\n" << std::endl;
@@ -15,7 +15,7 @@ void ShapeSorter::printByType(const std::vector<Shape *> &shapes,
 }
 
 void ShapeSorter::printBySide(const std::vector<Shape *> &shapes,
-                             const int &noOfSides) {
+                              const int &noOfSides) {
   for (const auto &shape : shapes) {
     if (shape->getNoOfSides() == noOfSides) {
       std::cout << shape->getType() << " has " << noOfSides << " sides\n"
@@ -27,20 +27,20 @@ void ShapeSorter::printBySide(const std::vector<Shape *> &shapes,
 void ShapeSorter::printByVolume(std::vector<Shape *> shapes) {
 
   std::sort(shapes.begin(), shapes.end(),
-            [](Shape *&a, Shape *&b) { return a->area() < b->area(); });
+            [](Shape *a, Shape *b) { return a->area() < b->area(); });
 
   for (const auto &shape : shapes) {
-    std::cout << shape->getType() << " " << shape->area() << "\n" <<  std::endl;
+    std::cout << shape->getType() << " " << shape->area() << "\n" << std::endl;
   }
 }
 
 void ShapeSorter::printByPerimeter(std::vector<Shape *> shapes) {
 
-  std::sort(shapes.begin(), shapes.end(), [](Shape *&a, Shape *&b) {
-    return a->perimeter() < b->perimeter();
-  });
+  std::sort(shapes.begin(), shapes.end(),
+            [](Shape *a, Shape *b) { return a->perimeter() < b->perimeter(); });
 
   for (const auto &shape : shapes) {
-    std::cout << shape->getType() << " " << shape->perimeter() << "\n" << std::endl;
+    std::cout << shape->getType() << " " << shape->perimeter() << "\n"
+              << std::endl;
   }
 }
