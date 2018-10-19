fc4f54786b0fb11bad114fc037b8da300ac45358
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2018-10-19 14:59:38 +0100

    Avoiding call to move

--- exercises/dolica_akelloegwel/ex02_oo_basics/src/main.cpp
+++ exercises/dolica_akelloegwel/ex02_oo_basics/src/main.cpp
@@ -11,26 +11,22 @@ int main(int, char **)
 {
 	// Create various shapes
 	double squareSide = 2.5;
-	std::unique_ptr<Square> square_ptr(new Square(squareSide));
 
 	double recWidth = 2;
 	double recHeight = 5;
-	std::unique_ptr<Rectangle> rectangle_ptr(new Rectangle(recWidth, recHeight));
 
 	double circleRadius = 5;
-	std::unique_ptr<Circle> circle_ptr(new Circle(circleRadius));
 
 	double triangleBase = 3;
 	double triangleHeight = 4;
-	std::unique_ptr<Triangle> triangle_ptr(new Triangle(triangleBase, triangleHeight));
 
 	// Create a vector and place the shapes in it
 	std::vector<std::unique_ptr<Shape>> myShapes;
 
-	myShapes.push_back(std::move(square_ptr));
-	myShapes.push_back(std::move(rectangle_ptr));
-	myShapes.push_back(std::move(circle_ptr));
-	myShapes.push_back(std::move(triangle_ptr));
+	myShapes.push_back(std::make_unique<Square>(squareSide));
+	myShapes.push_back(std::make_unique<Rectangle>(recWidth,recHeight));
+	myShapes.push_back(std::make_unique<Circle>(circleRadius));
+	myShapes.push_back(std::make_unique<Triangle>(triangleBase, triangleHeight));
 
 	// Examine behaviour of the shape sorter
 	ShapeSorter shapeSorter = ShapeSorter();
