4ad86e10edba4eeadc998d935016e442908d4b75
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2018-10-19 15:50:19 +0100

    Vector stores pointer with const Shape

--- exercises/dolica_akelloegwel/ex02_oo_basics/src/ShapeSorter.cpp
+++ exercises/dolica_akelloegwel/ex02_oo_basics/src/ShapeSorter.cpp
@@ -1,53 +1,53 @@
 #include "ShapeSorter.h"
 
-bool ShapeSorter::AreaComparator(const std::unique_ptr<Shape> &s1, const std::unique_ptr<Shape> &s2)
+bool ShapeSorter::AreaComparator(const std::unique_ptr<const Shape> &s1, const std::unique_ptr<const Shape> &s2)
 {
 	return s2->GetArea() < s1->GetArea() || (s1->GetArea() >= s2->GetArea() && s1->GetArea() < s2->GetArea());
 }
-bool ShapeSorter::PerimeterComparator(const std::unique_ptr<Shape> &s1, const std::unique_ptr<Shape> &s2)
+bool ShapeSorter::PerimeterComparator(const std::unique_ptr<const Shape> &s1, const std::unique_ptr<const Shape> &s2)
 {
 	return s2->GetPerimeter() < s1->GetPerimeter() || (s1->GetPerimeter() >= s2->GetPerimeter() && s1->GetPerimeter() < s2->GetPerimeter());
 }
-void ShapeSorter::PrintBasedOnType(const std::vector<std::unique_ptr<Shape>> &shapes, std::string type) const
+void ShapeSorter::PrintBasedOnType(const std::vector<std::unique_ptr<const Shape>> &shapes, std::string type) const
 {
 	// Print shapes that have a given type
 	for (auto &shape : shapes)
 		if (shape.get()->GetType() == type)
 			std::cout << *shape.get() << std::endl;
 }
-void ShapeSorter::PrintBasedOnNSides(const std::vector<std::unique_ptr<Shape>> &shapes, int numSides) const
+void ShapeSorter::PrintBasedOnNSides(const std::vector<std::unique_ptr<const Shape>> &shapes, int numSides) const
 {
 	// Print shapes that have a given number of sides
 	for (auto &shape : shapes)
 		if (shape.get()->GetNumSides() == numSides)
 			std::cout << *shape.get() << std::endl;
 }
-std::vector<std::unique_ptr<Shape>> ShapeSorter::SortByAreaDesc(const std::vector<std::unique_ptr<Shape>> &shapes) const
+std::vector<std::unique_ptr<const Shape>> ShapeSorter::SortByAreaDesc(const std::vector<std::unique_ptr<const Shape>> &shapes) const
 {
 	// Create a copy of the shapes vector
-	std::vector<std::unique_ptr<Shape>> copyShapes;
+	std::vector<std::unique_ptr<const Shape>> copyShapes;
 
 	for (auto &shape : shapes)
-     copyShapes.push_back(std::unique_ptr<Shape>(shape->clone()));
+     copyShapes.push_back(std::unique_ptr<const Shape>(shape->clone()));
 
 	// Sort the copied vector
 	std::sort(copyShapes.begin(), copyShapes.end(), AreaComparator);
 
 	return copyShapes;
 }
-std::vector<std::unique_ptr<Shape>> ShapeSorter::SortByPerimeterDesc(const std::vector<std::unique_ptr<Shape>> &shapes) const
+std::vector<std::unique_ptr<const Shape>> ShapeSorter::SortByPerimeterDesc(const std::vector<std::unique_ptr<const Shape>> &shapes) const
 {
-	std::vector<std::unique_ptr<Shape>> copyShapes;
+	std::vector<std::unique_ptr<const Shape>> copyShapes;
 
   for (auto &shape : shapes)
-    copyShapes.push_back(std::unique_ptr<Shape>(shape->clone()));
+    copyShapes.push_back(std::unique_ptr<const Shape>(shape->clone()));
 
 	// Sort the copied vector
 	std::sort(copyShapes.begin(), copyShapes.end(), PerimeterComparator);
 
 	return copyShapes;
 }
-void ShapeSorter::PrintShapes(const std::vector<std::unique_ptr<Shape>> &shapes) const
+void ShapeSorter::PrintShapes(const std::vector<std::unique_ptr<const Shape>> &shapes) const
 {
 	for (auto &shape : shapes)
 		std::cout << *shape.get() << std::endl;
--- exercises/dolica_akelloegwel/ex02_oo_basics/src/ShapeSorter.h
+++ exercises/dolica_akelloegwel/ex02_oo_basics/src/ShapeSorter.h
@@ -7,12 +7,12 @@
 class ShapeSorter
 {
 public:
-	static bool AreaComparator(const std::unique_ptr<Shape> &s1, const std::unique_ptr<Shape> &s2);
-	static bool PerimeterComparator(const std::unique_ptr<Shape> &s1, const std::unique_ptr<Shape> &s2);
-	void PrintBasedOnType(const std::vector<std::unique_ptr<Shape>> &shapes, std::string type) const;
-	void PrintBasedOnNSides(const std::vector<std::unique_ptr<Shape>> &shapes, int numSides) const;
-	std::vector<std::unique_ptr<Shape>> SortByAreaDesc(const std::vector<std::unique_ptr<Shape>> &shapes) const;
-	std::vector<std::unique_ptr<Shape>> SortByPerimeterDesc(const std::vector<std::unique_ptr<Shape>> &shapes) const;
-	void PrintShapes(const std::vector<std::unique_ptr<Shape>> &shapes) const;
+	static bool AreaComparator(const std::unique_ptr<const Shape> &s1, const std::unique_ptr<const Shape> &s2);
+	static bool PerimeterComparator(const std::unique_ptr<const Shape> &s1, const std::unique_ptr<const Shape> &s2);
+	void PrintBasedOnType(const std::vector<std::unique_ptr<const Shape>> &shapes, std::string type) const;
+	void PrintBasedOnNSides(const std::vector<std::unique_ptr<const Shape>> &shapes, int numSides) const;
+	std::vector<std::unique_ptr<const Shape>> SortByAreaDesc(const std::vector<std::unique_ptr<const Shape>> &shapes) const;
+	std::vector<std::unique_ptr<const Shape>> SortByPerimeterDesc(const std::vector<std::unique_ptr<const Shape>> &shapes) const;
+	void PrintShapes(const std::vector<std::unique_ptr<const Shape>> &shapes) const;
 
 };
