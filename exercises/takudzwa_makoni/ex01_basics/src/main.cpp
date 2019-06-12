56e5a8ef91d7cb8f51e131ac46f326678c424503
Takudzwa <takudzwamilli@gmail.com>
2019-06-12 16:12:58 +0100

    exercise01

---
+++ exercises/takudzwa_makoni/ex01_basics/src/main.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+int main()
+{
+	ofstream testFile;
+	testFile.open("Holmes.txt");
+	string token;
+
+
+
+
+	testFile.close();
+
+}
+
