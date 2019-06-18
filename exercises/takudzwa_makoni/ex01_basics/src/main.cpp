75cc46fc9743dd7b2367ca67c24741f83b2bf00b
Takudzwa <takudzwamilli@gmail.com>
2019-06-18 16:58:11 +0100

    using std::any_of instead of switch/case in isSpecialCharacter as per review.

--- exercises/takudzwa_makoni/ex01_basics/src/main.cpp
+++ exercises/takudzwa_makoni/ex01_basics/src/main.cpp
@@ -10,26 +10,32 @@
 std::vector<std::string> searchList = { "which","holmes","there","could","photograph" }; // list of words to be searched.
 
 
+struct ComparableToSpecial {
+
+	ComparableToSpecial(char c) : inputChar(c) {}
+
+	const char inputChar;
+	bool operator()(char c) const { return c == inputChar;  }
+
+};
+
+
 // return true if special character exists in c.
 bool isSpecialCharacter(char c)
 {
-	switch (c)
-	{
-	case '(':
-	case ')':
-	case '.':
-	case ',':
-	case '"':
-	case '?':
-	case '!':
-	case '\'':
-	case ':':
-		return true;
-	default:
-		return false;
-	}
+	std::string specialChars("()!?,.\":\\");
+	std::string specialChar();
+	if (std::any_of(specialChars.begin(), specialChars.end(), ComparableToSpecial(c))) { return true; }
+	
 }
 
+
+struct ComparableTo {
+
+	std::string character;
+	ComparableTo(std::string ch) : character(ch) {};
+	bool operator()(std::string ch) const { return ch == character; }
+};
 
 //count number of occurences in file and cout result
 void printCount(const std::string& searchWord, std::ifstream& fileObj) {
@@ -41,8 +47,10 @@ void printCount(const std::string& searchWord, std::ifstream& fileObj) {
 	std::string word; //word from file
 	while (fileObj >> word) {
 	
-		std::transform(word.begin(), word.end(), word.begin(), [](unsigned char c) -> unsigned char { return std::tolower(c); }); //std::tolower); //convert all characters of word to lower
-		std::remove_if(word.begin(), word.end(), &isSpecialCharacter); //remove special characters TODO possible implementation of std::any_of
+		std::transform(word.begin(), word.end(), word.begin(), [](unsigned char c) -> unsigned char { return std::tolower(c); }); 
+		std::remove_if(word.begin(), word.end(), &isSpecialCharacter); 
+
+	
 		if (word == searchWord) {
 			++count;
 		}
