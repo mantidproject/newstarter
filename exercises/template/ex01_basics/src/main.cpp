1a07a269e5a739c358bad36323f597efc974bd0b
Adam Washington <rprospero@gmail.com>
2019-08-09 09:32:19 +0100

    Check command line argument

--- exercises/template/ex01_basics/src/main.cpp
+++ exercises/template/ex01_basics/src/main.cpp
@@ -2,6 +2,15 @@
  * Skeleton main routine
  */
 
-int main(int, char **)
+#include <iostream>
+
+int main(int argc, char **argv)
 {
+  if(argc <= 1) {
+    std::cout << "Missing file name" << std::endl;
+    return -1;
+  }
+  std::cout << "Found file" << std::endl;
+  std::cout << argv[1] << std::endl;
+  return 0;
 }
