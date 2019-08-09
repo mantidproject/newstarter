35d09c39ff22a83be712213a12ad56f0815c675e
Adam Washington <rprospero@gmail.com>
2019-08-09 17:23:02 +0100

    Start second project

--- exercises/template/ex02_oo_basics/src/main.cpp
+++ exercises/template/ex02_oo_basics/src/main.cpp
@@ -2,6 +2,46 @@
  * Skeleton main routine
  */
 
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+class Shape {
+public:
+  virtual int sides() = 0;
+  virtual double perimeter() = 0;
+  virtual double area() = 0;
+  virtual string type() = 0;
+};
+
+class Circle : public Shape {
+private:
+  double radius;
+public:
+  explicit Circle(double r) {
+    radius = r;
+  }
+  ~Circle() { }
+  int sides() override {
+    return 1;
+  }
+  double perimeter() override {
+    return 2 * M_PI * radius;
+  }
+  double area() override {
+    return M_PI * radius * radius;
+  }
+  string type() override {
+    return "circle";
+  }
+};
+
 int main(int, char **)
 {
+  Circle trial(5);
+  cout << "Hello, World!" << endl;
+  cout << trial.sides() << endl;
+  cout << trial.perimeter() << endl;
+  cout << trial.area() << endl;
 }
