98124f4432d141e612ab02917fa575763eafa3e3
Adam Washington <rprospero@gmail.com>
2019-08-09 10:03:27 +0100

    Stl base check for missing files

--- exercises/template/ex01_basics/src/main.cpp
+++ exercises/template/ex01_basics/src/main.cpp
@@ -2,9 +2,14 @@
  * Skeleton main routine
  */
 
+#include <algorithm>
 #include <boost/filesystem.hpp>
 #include <iostream>
 #include <vector>
+
+bool fileMissing(std::string f) {
+  return !boost::filesystem::exists(f);
+}
 
 int main(int argc, char **argv)
 {
@@ -12,12 +17,18 @@ int main(int argc, char **argv)
     std::cout << "Missing file name" << std::endl;
     return -1;
   }
+
   std::vector<std::string> arguments(argv+1, argc+argv);
-  for (boost::filesystem::path arg : arguments) {
-    if (!boost::filesystem::exists(arg)) {
-      std::cout << arg << " does not exist" << std::endl;
-      break;
-    }
+
+  auto missing = std::find_if(arguments.begin(), arguments.end(), fileMissing);
+
+  if (missing != arguments.end()) {
+    std::cout << "Cannot find file " << *missing << std::endl;
+    return -1;
+  }
+
+  for (std::string arg : arguments) {
+    std::cout << arg << std::endl;
   }
   return 0;
 }
