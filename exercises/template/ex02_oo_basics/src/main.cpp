bcbb3c9490ee8575ad405737a10ca9e5682f86ad
Adam Washington <adam.washington@stfc.ac.uk>
2019-08-12 11:30:22 +0100

    Autoformat code

--- exercises/template/ex02_oo_basics/src/main.cpp
+++ exercises/template/ex02_oo_basics/src/main.cpp
@@ -28,131 +28,109 @@ public:
 class Circle : public Shape {
 private:
   double radius;
+
 public:
-  explicit Circle(double r) {
-    radius = r;
-  }
-  int sides() override {
-    return 1;
-  }
-  double perimeter() override {
-    return 2 * M_PI * radius;
-  }
-  double area() override {
-    return M_PI * radius * radius;
-  }
-  string type() override {
-    return "circle";
-  }
+  explicit Circle(double r) { radius = r; }
+  int sides() override { return 1; }
+  double perimeter() override { return 2 * M_PI * radius; }
+  double area() override { return M_PI * radius * radius; }
+  string type() override { return "circle"; }
 };
 
 class Square : public Shape {
 private:
   double side;
+
 public:
-  explicit Square(double s) {
-    side = s;
-  }
-  int sides() override {
-    return 4;
-  }
-  double perimeter() override {
-    return 4 * side;
-  }
-  double area() override {
-    return side*side;
-  }
-  string type() override {
-    return "square";
-  }
+  explicit Square(double s) { side = s; }
+  int sides() override { return 4; }
+  double perimeter() override { return 4 * side; }
+  double area() override { return side * side; }
+  string type() override { return "square"; }
 };
 
 class Rectangle : public Shape {
 private:
   double width;
   double height;
+
 public:
   explicit Rectangle(double w, double h) {
     width = w;
     height = h;
   }
-  int sides() override {
-    return 4;
-  }
-  double perimeter() override {
-    return 2 * (width + height);
-  }
-  double area() override {
-    return width*height;
-  }
-  string type() override {
-    return "rectangle";
-  }
+  int sides() override { return 4; }
+  double perimeter() override { return 2 * (width + height); }
+  double area() override { return width * height; }
+  string type() override { return "rectangle"; }
 };
 
 class Triangle : public Shape {
 private:
   double base;
   double height;
+
 public:
   explicit Triangle(double b, double h) {
     base = b;
     height = h;
   }
-  int sides() override {
-    return 3;
-  }
+  int sides() override { return 3; }
   double perimeter() override {
-    return base + 2 * sqrt(height*height + base*base/4);
+    return base + 2 * sqrt(height * height + base * base / 4);
   }
-  double area() override {
-    return 0.5*base*height;
-  }
-  string type() override {
-    return "triangle";
-  }
+  double area() override { return 0.5 * base * height; }
+  string type() override { return "triangle"; }
 };
 
 class ShapeSorter {
 private:
   vector<shared_ptr<Shape>> contents;
+
 public:
-  explicit ShapeSorter(vector<shared_ptr<Shape>> &c) : contents(c) {};
+  explicit ShapeSorter(vector<shared_ptr<Shape>> &c) : contents(c){};
   void print() {
-    //print all shapes
+    // print all shapes
     for (auto &shape : contents) {
       shape->print();
     }
   }
   void sides(int side_count) {
-    //print shapes with given number of sides
-    for_each(contents.begin(), contents.end(), [side_count](shared_ptr<Shape> value){if (value->sides() == side_count) value->print();});
+    // print shapes with given number of sides
+    for_each(contents.begin(), contents.end(),
+             [side_count](shared_ptr<Shape> value) {
+               if (value->sides() == side_count)
+                 value->print();
+             });
   }
   void typed(const string &type) {
-    //print shapes of a given type
-    for_each(contents.begin(), contents.end(), [type](shared_ptr<Shape> value){if (value->type() == type) value->print();});
+    // print shapes of a given type
+    for_each(contents.begin(), contents.end(), [type](shared_ptr<Shape> value) {
+      if (value->type() == type)
+        value->print();
+    });
   }
 
   void area_sort() {
-    //Sort contents by area and print
+    // Sort contents by area and print
     sort(contents.begin(), contents.end(),
-	 [](shared_ptr<Shape> a, shared_ptr<Shape> b) { return a->area() < b->area(); });
+         [](shared_ptr<Shape> a, shared_ptr<Shape> b) {
+           return a->area() < b->area();
+         });
     print();
   }
 
   void perimeter_sort() {
-    //Sort contents by perimeter and print
+    // Sort contents by perimeter and print
     sort(contents.begin(), contents.end(),
-	 [](shared_ptr<Shape> a, shared_ptr<Shape> b) {
-	   return a->perimeter() < b->perimeter();
-	 });
+         [](shared_ptr<Shape> a, shared_ptr<Shape> b) {
+           return a->perimeter() < b->perimeter();
+         });
     print();
   }
-
 };
 
-int main(int, char **)
-{
+int main(int, char **) {
   vector<shared_ptr<Shape>> shapes;
   shapes.push_back(shared_ptr<Shape>(new Circle(5)));
   shapes.push_back(shared_ptr<Shape>(new Square(5)));
