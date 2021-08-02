b15935e967b3ae4556cdbb923ab049ed7ad970ed
EthanDevlinSTFC <ethan.devlin@stfc.ac.uk>
2021-08-02 11:53:48 +0100

    testing readfile

--- exercises/EthanDevlin/ex01_basics/src/main.cpp
+++ exercises/EthanDevlin/ex01_basics/src/main.cpp
@@ -1,7 +1,15 @@
-/**
- * Skeleton main routine
- */
+#include <iostream>
+#include <fstream>
+#include <string>
 
-int main(int, char **)
+int main(std::string fileName)
 {
+    std::string inLine;
+    std::ifstream inFile(fileName);
+
+    while (getline(inFile,inLine))
+    {
+        std::cout << inLine;
+    }
+    inFile.close();
 }
