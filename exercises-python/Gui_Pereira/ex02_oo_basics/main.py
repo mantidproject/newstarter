6f084336efd05049d32d1b65b541375f1dae8c2b
Gui Pereira <gui.maciel-pereira@stfc.ac.uk>
2023-09-11 15:57:41 +0100

    Edited main.py to start second exercise, currently unfinished.

---
+++ exercises-python/Gui_Pereira/ex02_oo_basics/main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+class Square:
+    def __init__(self, side1):
+        self.side1 = side1
+        self.side2 = side1
+    sides = 2
+
+class Rectangle:
+    def __init__(self, side1, side2):
+        self.side1 = side1
+        self.side2 = side2
+    sides = 2
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    sides = 0
+     
+class Triangle:
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+    sides = 3
+
+class ShapeSorter:
+
+    def __init__(self, shapes):
+        self.shapes = shapes
+
+    def get_
+def main():
+
+
+
+if __name__ == "__main__":
+	main()
