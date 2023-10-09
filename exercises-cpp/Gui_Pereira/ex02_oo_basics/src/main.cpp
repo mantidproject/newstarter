56aa91531f9ad77a1e521b4ee9fb067f972bde6c
GuiMacielPereira <gui.maciel-pereira@stfc.ac.uk>
2023-10-09 14:36:45 +0100

    Formatted code, added consts, passed by pointers and references
    Installed cpplinter from Google and formatted my code accordingly.
    Added const throughout function arguments and function attributes.
    Split some big functions into separate functions.
    Passed arguments either by reference or pointers.

--- exercises-cpp/Gui_Pereira/ex02_oo_basics/src/main.cpp
+++ exercises-cpp/Gui_Pereira/ex02_oo_basics/src/main.cpp
@@ -6,66 +6,63 @@
 #include<vector>
 #include <algorithm>
 
-
 class Polygon {
-    protected:
-    int height;
-    int base;
-    int radius;
-    std::string type;
-    Polygon (int h, int b) : height {h}, base {b} {};
-    Polygon (int r) : radius{r} {};
+ protected:
+  int height {1};
+  int base {1};
+  int radius {1};
+  std::string type;
+  Polygon(int h, int b) : height {h}, base {b} {};
+  explicit Polygon(int r) : radius{r} {};
 
-    public:
-    virtual double get_area () =0;
-    std::string get_type () {return type;}
-    void print () {std::cout << type << " " << get_area() << '\n';}
+ public:
+  virtual int get_area() const = 0;
+  std::string get_type () const {return type;}
+  void print() const {std::cout << type << " " << get_area() << '\n';}
 };
 
 class Circle : public Polygon {
-    public:
-    Circle (int r) : Polygon(r) {type = "Circle";};
-    double get_area () {return 3.14*radius*radius;};
+ public:
+  explicit Circle(int r) : Polygon(r) {type = "Circle";}
+  int get_area () const {return 3.14*radius*radius;}
 };
 
 class Rectangle: public Polygon {
-    public:
-    Rectangle (int h, int b) : Polygon(h, b) {type = "Rectangle";};
-    double get_area () {return height*base;};
+ public:
+  Rectangle(int h, int b) : Polygon(h, b) {type = "Rectangle";}
+  int get_area() const {return height*base;}
 };
 
 class Square: public Polygon {
-    public:
-    Square (int h) : Polygon(h, h) {type = "Square";};
-    double get_area () {return height*base;};
+ public:
+  explicit Square(int h) : Polygon(h, h) {type = "Square";}
+  int get_area () const {return height*base;}
 };
 
 class Triangle: public Polygon {
-    public:
-    Triangle (int h, int b) : Polygon(h, b) {type = "Triangle";};
-    double get_area () {return height*base/2;};
+ public:
+  Triangle(int h, int b) : Polygon(h, b) {type = "Triangle";}
+  int get_area () const {return height*base/2;}
 };
 
 class ShapeSorter {
+ public:
+  ShapeSorter() {}
 
-    public:
-    ShapeSorter() {};       // Chose not to include shapes here becuase I do not have a default initializer for Polygon()
+  void sort_by_area(std::vector<Polygon*> *shapes) {
+    std::sort(shapes->begin(), shapes->end(), [](auto l, auto r){
+        return l->get_area() < r->get_area();
+        });
+  }
 
-    void sort_by_area(std::vector<Polygon*> &shapes) {      // Modifies the vector by reference, not the best solution in this case
-        std::sort(shapes.begin(), shapes.end(), [](auto l, auto r){return l->get_area() < r->get_area();});
-    };
-
-    void print_shapes_with_type(const std::vector<Polygon*> &shapes, std::string type) {
-        for (Polygon* p : shapes) if (p->get_type()==type) p->print();
-    }
-
-    // To implement sort by perimeter, would follow same logic as for area
-    // Would use the same technique as I implemented type to implement number of sides? Is there a more automatic alternative?
+  void print_shapes_with_type(
+      const std::vector<Polygon*> *shapes, std::string type
+      ) {
+      for (auto const& p : *shapes) if (p->get_type() == type) p->print();
+  }
 };
 
-int main(int, char **)
-{
-    //
+int main(int, char **) {
     Circle c1 = Circle(3);
     Circle c2 = Circle(5);
     Rectangle r1 {3, 4};
@@ -80,13 +77,14 @@ int main(int, char **)
     ShapeSorter SS{};
 
     std::cout << "Original shape vector:\n";
-    for (Polygon* p : shapes) p->print(); 
+    for (auto const& p : shapes) p->print();
 
-    SS.sort_by_area(shapes);
+    std::vector<Polygon*> *shapes_p = &shapes;  // Init pointer
+    SS.sort_by_area(shapes_p);
 
     std::cout << "\nSorted shape vector:\n";
-    for (Polygon* p : shapes) p->print(); 
+    for (auto const& p : shapes) p->print();
 
     std::cout << "\nSingle type shape vector:\n";
-    SS.print_shapes_with_type(shapes, "Rectangle");
+    SS.print_shapes_with_type(shapes_p, "Rectangle");
 }
